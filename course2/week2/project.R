c78f38b688dc5ba1d8d11a2f62b684f0b3128d02
virajbhalala <viraj.bhalala@optum.com>
2017-01-26 11:08:42 -0500

    working on cource 2 week 3 assignment

--- course2/week2/project.R
+++ course2/week2/project.R
@@ -33,6 +33,7 @@ corr <- function(directory, threshold = 0) {
   corrr = numeric()
   for (i in ids) {
     
+    
     newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"), 
                              ".csv", sep = ""))
     dff = newRead[complete.cases(newRead), ]
